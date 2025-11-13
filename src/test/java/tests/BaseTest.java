b612c367d9d12590de18e169d17be59d7bd54b9f
lenagithub4 <lena-usc@inbox.lv>
2025-11-13 21:25:03 +0200

    my 1st commit

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -22,7 +22,7 @@ public abstract class BaseTest {
     @BeforeClass
     public void setUp() {
         driver = DriverFactory.getDriver(PropertyReader.getBrowser());
-        //driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(3, TimeUnit. SECONDS);
         driver.get(PropertyReader.getUrl());
         steps = new SearchSteps();
     }
